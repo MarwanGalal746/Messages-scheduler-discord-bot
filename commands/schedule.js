8bc140635ed10def84dbe1fcd6760f2f943d9ef5
Marwan Galal <marawangalal84@gmail.com>
2021-05-26 02:06:49 +0200

    rep

--- commands/schedule.js
+++ commands/schedule.js
@@ -28,64 +28,40 @@ module.exports = {
                 }
                 channel.send(content)
             }
-            let del= {
-                date: {
-                    $lte: Date.now()
-                },
-                repetition: 'no'
-            }
-            await schMessages.deleteMany(del)
-            del= {
-                date: {
-                    $lte: Date.now()
-                },
-                repetition: {
-                    $eq: 'daily'
-                }
-            }
-           await schMessages.collection.updateMany({
-            date: {
-                $lte: Date.now()
-            },
-            repetition: {
-                $eq: 'daily'
-            }
-        },{$set : {"date":Date.now() +1}}, { upsert: true } )
-            //await schMessages.where({repetition: 'daily'}).update({date: Date.now()+1})
+
+            await schMessages.deleteMany(query)
             setTimeout(checkForPosts, 10000);
         } 
         checkForPosts()
     },
     callback: async ({message, args}) => {
-        console.log()
         const {mentions, guild, channel} = message
         const targetChannel = mentions.channels.first()
         if(!targetChannel){
-            console.log('Please tag a channel to send your message in, to learn how to use the bot type !help.')
+            console.log('Please tag a channel to send your message in.')
             return
         }
         args.shift()
         const [date, time, clockType, timeZone, rep] = args
         if(clockType!=='AM'  &&  clockType!=='PM') {
-            console.log(`Please provide either "AM" or "PM", to learn how to use the bot type !help.`)
+            console.log(`Please provide either "AM" or "PM" .`)
             return
         }
 
         const validTZs = momentTimezone.tz.names()
         if(!validTZs.includes(timeZone)){
-            console.log("Please provide known timezone, to learn how to use the bot type !help.")
+            console.log("Please provide known timezone.")
             return
         }
-
-
 
         const targetDate = momentTimezone.tz(
             `${date} ${time} ${clockType}`,
             "YYYY/MM/DD HH:mm A",
             timeZone
         )
-        if(rep !== 'no' && rep !== 'daily' && rep !== 'weekly'){
-            console.log("Please provide a valid repetition, to learn how to use the bot type !help. ")
+        console.log(rep)
+        if(rep!=='no' && rep!=='daily'){
+            console.log('Please enter a valid rep')
             return
         }
 
@@ -107,24 +83,14 @@ module.exports = {
                 return
             }
             message.reply("Your message has been scheduled")
-            console.log(collectedMessage.content,guild.id,targetChannel.id, typeof(targetChannel.id))
-            if(rep==='no'){
-                await new schMessages({
-                    date:targetDate.valueOf(),
-                    content: collectedMessage.content,  
-                    guildId: guild.id,
-                    channelId: targetChannel.id,
-                    repetition: "no"
-                }).save()
-            } else if(rep==='daily'){
-                await new schMessages({
-                    date:targetDate.valueOf(),
-                    content: collectedMessage.content,  
-                    guildId: guild.id,
-                    channelId: targetChannel.id,
-                    repetition: "daily"
-                }).save()
-            } 
+            console.log(collectedMessage.content,guild.id.length,targetChannel.id, typeof(targetChannel.id))
+            await new schMessages({
+                date:targetDate.valueOf(),
+                content: collectedMessage.content,  
+                guildId: guild.id,
+                channelId: targetChannel.id,
+                repetition: rep
+            }).save()
         })
     }
 }
